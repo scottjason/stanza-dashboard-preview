fa0020b85fe48c2e6887704eb2a6bb74e9e6aa4d
Scott Jason <scottleejason@gmail.com>
2015-09-23 22:12:55 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Stanza Dashboard Preview | September 2015
+
+Yash Saxena, Scott Jason, Jonathan Capecchi
+
+http://stanza-dashboard.herokuapp.com
---
+++ public/js/init.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  var module = {};
+
+  module.bindListeners = function(cb) {
+    document.getElementById('preview').addEventListener('click', module.renderPreview, false);
+    document.getElementById('stanza-site').addEventListener('click', module.viewStanza, false);
+  };
+
+  module.renderPreview = function(event) {
+    var imgLink = event.target.currentSrc;
+    window.open(imgLink);
+  };
+
+  module.viewStanza = function() {
+    var url = 'https://stanza.co/';
+    window.open(url);
+  };
+  module.bindListeners(module.startCarousel);
+});
